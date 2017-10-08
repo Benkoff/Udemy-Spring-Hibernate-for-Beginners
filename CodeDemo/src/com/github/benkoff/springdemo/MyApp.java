a36f88d82a4d1d1ee727aa2c65ad540207435c2b
Benkoff <bigopendatanet@yahoo.com>
2017-10-09 02:25:49 +0300

    going on

--- CodeDemo/src/com/github/benkoff/springdemo/MyApp.java
+++ CodeDemo/src/com/github/benkoff/springdemo/MyApp.java
@@ -3,7 +3,7 @@ package com.github.benkoff.springdemo;
 public class MyApp {
 
 	public static void main(String[] args) {
-		Coach coach = new BaseballCoach();
+		Coach coach = new TrackCoach();
 		System.out.println(coach.getDailyWorkout());
 	}
 
